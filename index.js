f95db9402d2c33f3b32d5504722647054f47db22
Jenna <116000928+jnnhan@users.noreply.github.com>
2023-08-09 20:43:30 +0300

    Add REST Client requests and exercises 3.2-3.6

--- index.js
+++ index.js
@@ -26,6 +26,58 @@ let persons = [
     }
 ]
 
+const generateId = () => {
+    const newId = Math.floor(Math.random() * 1001)
+    return newId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number is missing'
+        })
+    } else if (persons.some(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+
+    response.json(persons.filter(person => person.id !== id))
+    response.status(204).end()
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    const info = `<p>Phonebook has info for ${persons.length} people</p>`
+    let time = new Date()
+
+    response.send(info + time)
+})
+
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook</h1>')
 })
